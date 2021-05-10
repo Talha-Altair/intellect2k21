901a95197915af13ccb37fadcc47127ee4c5aa6e
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-05-11 02:16:38 +0530

    Secret API added

--- app.py
+++ app.py
@@ -1,14 +1,22 @@
-from flask import Flask, render_template #Imports Flask Module
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
-@app.route('/about') #Defining Root Node
+@app.route('/about') 
 def hello_world():
     return render_template('about.html')
 
 
-@app.route('/') #Second Node
+@app.route('/secret',methods=["POST","GET"])
 def hello_world1():
-    return 'You are in 3nd Node'    
+    return render_template('secret.html')    
+
+@app.route('/saponihimym',methods=["POST","GET"])    
+def hello_world2():
+    key = request.form.get("secret")
+    print(key)
+    if (key=='pass'):
+        return 'Acess Granted'
+    return 'Authentication Failed'    
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True) #Port is Set to 8080 and Host is set to Global
+  app.run(host='0.0.0.0', port=8080, debug=True)
