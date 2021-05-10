e8ca843cb83a5a3d78c3fb85faf1ba8b2413f6d3
Talha Abdur Rahman <cooltalha27@gmail.com>
2021-05-11 02:25:09 +0530

    Updates for 2021-05-11

--- app.py
+++ app.py
@@ -10,12 +10,18 @@ def hello_world():
 def hello_world1():
     return render_template('secret.html')    
 
-@app.route('/saponihimym',methods=["POST","GET"])    
+"""
+
+http://0.0.0.0:8080/api/saponihimym
+
+"""
+
+@app.route('/api/saponihimym',methods=["POST","GET"])    
 def hello_world2():
-    key = request.form.get("secret")
+    key = request.values.get("secret")
     print(key)
     if (key=='pass'):
-        return 'Acess Granted'
+        return 'Access Granted'
     return 'Authentication Failed'    
 
 if __name__ == '__main__':
