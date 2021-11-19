e685cbf74400b693814789f24df13c1009f5b12e
Talha-Altair <cooltalha27@gmail.com>
2021-11-19 11:05:46 +0530

    Updates for 2021-11-19 11:05:46

--- app.py
+++ app.py
@@ -82,4 +82,4 @@ def hello_world2():
     return render_template('404.html')    
 
 if __name__ == '__main__':
-  app.run(host='0.0.0.0', port=8080, debug=True)
+  app.run(host='0.0.0.0', port=8080, debug=False)
